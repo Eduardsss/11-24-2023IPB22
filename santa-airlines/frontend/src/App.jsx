dc3e7de8c25820f9e97d8d793120917f646d9e5f
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-12-14 12:18:27 +0200

    mans darbs

---
+++ santa-airlines/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import styles from './App.module.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p>Hello from fornt end</p>
+    </>
+  )
+}
+
+export default App
